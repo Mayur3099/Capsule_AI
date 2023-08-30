eeaecf03e3c5c471fc0089f2a506f5d880d6f854
Mayur3099 <139243145+Mayur3099@users.noreply.github.com>
2023-08-30 16:24:20 +0530

    first commit

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { v2 as cloudinary } from 'cloudinary';
+// import { v2 as cloudinary } from 'cloudinary';
 
 import Post from '../mongodb/models/post.js';
 
@@ -8,11 +8,11 @@ dotenv.config();
 
 const router = express.Router();
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 router.route('/').get(async (req, res) => {
     try {
